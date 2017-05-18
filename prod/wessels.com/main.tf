18c9b5645c5060000b58c6f30087effff42eec85
BerndWessels <wesselsbernd@gmail.com>
2017-05-18 16:04:32 +1200

    Consolidated platform-api and website server.

--- prod/wessels.com/main.tf
+++ prod/wessels.com/main.tf
@@ -3,7 +3,7 @@
  * -------------------------------------------------------------------------------------------------------------------
  */
 terraform {
-  required_version = "0.9.3"
+  required_version = "0.9.5"
   backend "s3" {
     bucket = "wessels.nz.terraform"
     key = "prod.wessels.nz.tfstate"
@@ -62,23 +62,6 @@ resource "aws_iam_role_policy" "platform_api_website_lambda" {
 }
 
 /**
- * IAM Role for the Platform GraphQL Lambda Function.
- */
-resource "aws_iam_role" "platform_api_graphql_lambda" {
-  name = "platform_api_graphql_lambda"
-  assume_role_policy = "${file("${path.module}/IAM/Lambda/PlatformAPIGraphQL/AssumeRolePolicy.json")}"
-}
-
-/**
- * IAM Role Policy for the Platform GraphQL Lambda Function.
- */
-resource "aws_iam_role_policy" "platform_api_graphql_lambda" {
-  name = "platform_api_graphql_lambda"
-  role = "${aws_iam_role.platform_api_graphql_lambda.id}"
-  policy = "${file("${path.module}/IAM/Lambda/PlatformAPIGraphQL/InlinePolicy.json")}"
-}
-
-/**
  * API Gateway
  * -------------------------------------------------------------------------------------------------------------------
  */
@@ -105,6 +88,8 @@ module "platform_domain" {
   email_domain_verification_cname_name = "${var.email_domain_verification_cname_name}"
   email_domain_verification_cname_value = "${var.email_domain_verification_cname_value}"
   email_mx_records = "${var.email_mx_records}"
+  cloudfront_distribution_domain_name = "${module.platform_api.cloudfront_distribution_domain_name}"
+  cloudfront_distribution_hosted_zone_id = "${module.platform_api.cloudfront_distribution_hosted_zone_id}"
 }
 
 /**
@@ -112,75 +97,11 @@ module "platform_domain" {
  */
 module "platform_api" {
   source = "git::git@github.com:wessels-nz/infrastructure-modules.git//platform-api"
+  region = "${var.aws_region}"
   stage_name = "${var.platform_api_state_name}"
-  stage_description = "${null_resource.platform_api_deployment.triggers.stage_description}"
-}
-
-/**
- * Create the Website endpoint for the Platform API.
- */
-module "platform_api_website" {
-  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-serverless-express"
-  region = "${var.aws_region}"
-  filename = "${path.module}/Lambda/platform-api-website/bundle.zip"
-  function_name = "platform-api-website"
-  role_arn = "${aws_iam_role.platform_api_website_lambda.arn}"
-  handler = "index.handler"
-  runtime = "nodejs6.10"
-  rest_api_id = "${module.platform_api.rest_api_id}"
-  rest_api_parent_resource_id = "${module.platform_api.rest_api_root_resource_id}"
-  create_child_resource = false
-}
-
-/**
- * Create the GraphQL endpoint for the Platform API.
- */
-module "platform_api_graphql" {
-  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-serverless-express"
-  region = "${var.aws_region}"
-  filename = "${path.module}/Lambda/platform-api-graphql/bundle.zip"
-  function_name = "platform-api-graphql"
-  role_arn = "${aws_iam_role.platform_api_graphql_lambda.arn}"
-  handler = "index.handler"
-  runtime = "nodejs6.10"
-  rest_api_id = "${module.platform_api.rest_api_id}"
-  rest_api_parent_resource_id = "${module.platform_api.rest_api_root_resource_id}"
-  create_child_resource = true
-  child_resource_path = "graphql"
-}
-
-/**
- * Create the S3 Bucket and Endpoint for the Platform Resources. TODO Files need to be copied manually at the moment.
- */
-module "platform_bucket_website_resources" {
-  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-s3-website"
-  bucket_name = "wessels.nz"
-}
-
-/**
- * Create the CloudFront Distribution for the Platform Website.
- */
-module "platform_distribution" {
-  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//platform-distribution"
-  aliases = ["${var.platform_domain_name}"]
-  root_origin_id = "platform_api_website"
-  root_origin_domain_name = "${module.platform_api.deployment_invoke_url}"
-  resource_origin_id = "platform_s3_website"
-  resource_origin_domain_name = "${module.platform_bucket_website_resources.website_endpoint}"
+  stage_description = "${var.platform_domain_name}"
+  website_lambda_filename = "${path.module}/Lambda/PlatformApiWebsite/bundle.zip"
+  website_lambda_role_arn = "${aws_iam_role.platform_api_website_lambda.arn}"
+  domain_name = "${var.platform_domain_name}"
   domain_certificate_arn = "${var.platform_domain_certificate_arn}"
-}
-
-/**
- * Dependency Helpers
- * -------------------------------------------------------------------------------------------------------------------
- */
-
-/**
- * Dependency Helper to ensure that the API Deployment happens after all API Resources have been created.
- */
-resource "null_resource" "platform_api_deployment" {
-  triggers {
-    stage_description = "${var.platform_api_state_description}"
-  }
-  depends_on = ["module.platform_api_website", "module.platform_api_graphql"]
 }
--- prod/wessels.com/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "platfom_api_deployment_invoke_url" {
-  value = "${module.platform_api.deployment_invoke_url}"
-}
